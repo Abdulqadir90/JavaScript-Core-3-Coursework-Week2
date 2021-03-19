d8f448353162c37ced562cd1f88be24a8face300
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-03-19 02:05:11 +0000

    programming humour done

--- challenges/programmer-humour/humour.js
+++ challenges/programmer-humour/humour.js
@@ -1,11 +1,23 @@
 const funny = document.getElementById("funnyPhotos");
 
-    function programmersHumour() {
-        fetch(`https://xkcd.now.sh/?comic=latest`)
-        .then(function (response){
+function programmersHumour() {
+    fetch(`https://xkcd.now.sh/?comic=latest`)
+    .then((response) => {
+        console.log('response',response)
+        if (response.ok) {
             return response.json()
+        } else {
+            throw new Error(response.error)
         }
+    })
+    .then((data) => {
+        let img = document.createElement("img");
+        img.src = data.img;
+        funny.appendChild(img);
+    })
+    .catch((error) => {
+        console.log('SOMETHING WENT WRONG:', error);
+    });
+}
 
-       
-       
-        )};
+programmersHumour();
